905ce16b2d97a0a89b5c9e6db5f2630507426c05
William Reima <william.reima@gmail.com>
2024-06-16 23:53:16 +0300

    surveys initial styling

--- resources/js/Pages/Survey/Survey.vue
+++ resources/js/Pages/Survey/Survey.vue
@@ -13,6 +13,28 @@ defineProps<{
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl">Surveyyss</h2>
+
+            <div>
+                <ul>
+                    <li v-for="survey in surveys.data" :key="survey.id">
+                        <button
+                            class="mt2 mb-2 border border-white hover:border-2"
+                        >
+                            <div class="text-white text-xl">
+                                {{ survey.title }}
+                            </div>
+                            <div>
+                                {{ survey.description }}
+                            </div>
+                            <div class="flex justify-end">
+                                <p class="mr-2">
+                                    last modified: {{ survey.modified_at }}
+                                </p>
+                            </div>
+                        </button>
+                    </li>
+                </ul>
+            </div>
             <pre class="text-white">{{ JSON.stringify(surveys, null, 2) }}</pre>
         </template>
     </AuthenticatedLayout>
