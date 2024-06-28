62eef7c0ffe003fd531b3d4a06cb7eef84d7eb82
William Reima <william.reima@gmail.com>
2024-06-28 23:49:12 +0300

    Edit form init

---
+++ resources/js/Pages/Survey/Edit.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+
+<template>
+    <Head title="Edit survey" />
+    <AuthenticatedLayout>
+        <template #header>
+            
+        </template>
+    </AuthenticatedLayout>
+</template>
